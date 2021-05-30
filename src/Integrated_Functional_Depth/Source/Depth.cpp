e2f274988d1697b4ba90f84bf021dedce4d6e7ce
Michela Ceoloni <michela.ceoloni@mail.polimi.it>
2021-05-30 21:10:05 +0000

    bugs fixed in the function that computes the MHRD

--- src/Integrated_Functional_Depth/Source/Depth.cpp
+++ src/Integrated_Functional_Depth/Source/Depth.cpp
@@ -10,14 +10,14 @@ MHRD::ranking(const VectorXr& v) const
 {
 
 	VectorXr vsorted = v;
-  VectorXi vrank   = VectorXi::Zero(v.size());
+  	VectorXi vrank   = VectorXi::Zero(v.size());
 
 	std::sort(vsorted.data(), vsorted.data() + vsorted.size());
 
 	for(Eigen::Index i=0; i<v.size(); ++i){
 		auto it = std::find(v.data(), v.data() + v.size(), vsorted[i]);
 		if(vrank[it - v.data()] != 0){
-			it = std::find(it, v.data() + v.size(), vsorted[i]);
+			it = std::find(it+1, v.data() + v.size(), vsorted[i]);
 		}
 		vrank[it - v.data()] = i+1;
 	}
@@ -32,22 +32,31 @@ MHRD::compute_depth() const
 	VectorXr mhipo = VectorXr::Zero(this->n_);
 	VectorXr hrd   = VectorXr::Zero(this->n_);
 
-	for(Eigen::Index j=0; j < this->n_; ++j){
+	for(Eigen::Index j=0; j < this->p_; ++j){
 
 	  VectorXi rmat = ranking(m_.row(j));
-
+	  
+	  /*for(int k=0; k<rmat.size(); ++k){
+	    Rprintf("%i \n", rmat[k]);
+	  }*/
+	    
 		for(Eigen::Index i=0; i < this->n_; ++i){
-			mepi[i]  = mepi[i]  + this->n_      - rmat[i];
-			mhipo[i] = mhipo[i] + rmat[i] - 1;
+			mepi[i]  = mepi[i]  + this->n_ - rmat[i];
+			mhipo[i] = mhipo[i] + rmat[i]  - 1;
 		}
 	}
 
-	std::for_each(mepi.data(), mepi.data() + mepi.size(), [this] (Real & r) {r /= (double) this->n_;});
-	std::for_each(mhipo.data(), mhipo.data() + mhipo.size(), [this] (Real & r) {r /= (double) this->n_;});
+	std::for_each(mepi.data(), mepi.data() + mepi.size(), [this] (Real & r) {r /= (double) (this->n_ - 1);});
+	std::for_each(mhipo.data(), mhipo.data() + mhipo.size(), [this] (Real & r) {r /= (double) (this->n_ - 1);});
 
-	Eigen::Index it = 0;
-	std::for_each( hrd.data(), hrd.data() + hrd.size(), [&mepi, &mhipo, &it] (Real & r) {r = std::min(mepi[it++], mhipo[it++]);} );
-
+	// Eigen::Index it = 0;
+	/*std::for_each( hrd.data(), hrd.data() + hrd.size(), [&mepi, &mhipo, &it] (Real & r) {r = std::min(mepi[it++], mhipo[it++]);} );
+  */
+	
+	for(Eigen::Index i=0; i < this->n_; ++i){
+	  hrd[i] = std::min(mepi[i], mhipo[i]);
+	}
+	
 	return hrd;
 
 }
