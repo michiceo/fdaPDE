d265aa0d4f3fbe0a67f9fbbfbd00c26ae118884a
Michela Ceoloni <michela.ceoloni@mail.polimi.it>
2022-06-22 23:57:14 +0000

    bug fixed

--- src/Integrated_Functional_Depth/Source/IFD_Depth.cpp
+++ src/Integrated_Functional_Depth/Source/IFD_Depth.cpp
@@ -18,7 +18,6 @@ Depth::isnan_vector(const VectorXr& v) const
 const VectorXi
 Depth::ranking(const VectorXr& v) const
 {
-  //omp_set_num_threads(2);
 
   VectorXr vsorted = v;
   VectorXi vrank   = VectorXi::Zero(v.size());
@@ -28,20 +27,16 @@ Depth::ranking(const VectorXr& v) const
   std::map<double, int> ranks;
   int rank = 1;
 
-  //#pragma omp parallel for default(none) shared(ranks, v, vsorted) reduction(+:rank)
   for(Eigen::Index index = 0; index < v.size(); index++)
   {
     double element = vsorted[index];
-    //#pragma omp atomic read
     ranks[element] = rank;
     rank++;
   }
 
-  //#pragma omp parallel for default(none) shared(ranks, v, vrank)
   for(Eigen::Index index = 0; index < v.size(); index++)
   {
     double element = v[index];
-    //#pragma omp atomic read
     vrank[index] = ranks[element];
   }
 	return vrank;
@@ -70,9 +65,6 @@ MHRD::compute_depth() const
 		}
 	}
 
-	// std::for_each(mepi.data(), mepi.data() + mepi.size(), [this] (Real & r) {r /= (double) (this->n_ - 1);});
-	// std::for_each(mhipo.data(), mhipo.data() + mhipo.size(), [this] (Real & r) {r /= (double) (this->n_ - 1);});
-
 	for(Eigen::Index i=0; i < this->n_; ++i){
 		hrd[i] = std::min(mepi[i], mhipo[i]);
 	}
@@ -86,34 +78,21 @@ MHRD::compute_depth(UInt k) const
 	VectorXr mhipo = VectorXr::Zero(this->p_);
 	VectorXr hrd   = VectorXr::Zero(this->p_);
 
-  omp_set_num_threads(2);
-
-  // Real time1 = omp_get_wtime();
-
-  # pragma omp parallel for default(none) shared(mepi, mhipo, hrd, p_, n_, m_, k)
   for(Eigen::Index j=0; j < this->p_; ++j){
 
     VectorXi rmat = ranking(m_.row(j));
     UInt number_nan = isnan_vector(m_.row(j));
 
     if(this->n_ - number_nan - rmat[k] > 0 ){
-      #pragma omp atomic write
       mepi[j] = (double) (this->n_ - number_nan - rmat[k] + 1) / ((double) (this->n_ - number_nan)*this->p_);
       mhipo[j] = (double) (rmat[k]) / ((double) (this->n_ - number_nan)*this->p_);
 		}
 		else{
-      #pragma omp atomic write
 			mepi[j] = 0;
 			mhipo[j] = 0;
 		}
-    #pragma omp atomic write
 	  hrd[j] = std::min(mepi[j], mhipo[j]);
 	}
-
-  // Real time2 = omp_get_wtime();
-  //
-  // Real time12 = (time2-time1);
-	// Rprintf("time12: %d\n", time12);
 
 	return std::make_tuple(mepi, mhipo, hrd);
 }
@@ -147,30 +126,18 @@ MBD::compute_depth(UInt k) const
 {
 	VectorXr mbd   = VectorXr::Zero(this->p_);
 
-  omp_set_num_threads(2);
-
-  // Real time1 = omp_get_wtime();
-
-  //# pragma omp parallel for default(none) shared(mbd, p_, n_, m_, k)
 	for(Eigen::Index j=0; j < this->p_; ++j){
 
     VectorXi rmat = ranking(m_.row(j));
     UInt number_nan = isnan_vector(m_.row(j));
 
     if(this->n_ - number_nan - rmat[k] >= 0 ){
-      //#pragma omp atomic write
       mbd[j] = (double) ((this->n_ - number_nan - rmat[k])*(rmat[k] - 1) + (this->n_ - number_nan - 1)) / ((double) (this->n_ - number_nan)*(this->n_ - number_nan - 1)*this->p_/2);
 		}
     else{
-      //#pragma omp atomic write
 			mbd[j] = 0;
 		}
   }
-
-  // Real time2 = omp_get_wtime();
-  //
-  // Real time12 = time2-time1;
-	// Rprintf("time12: %lf\n", time12);
-
+  
 	return mbd;
 }
